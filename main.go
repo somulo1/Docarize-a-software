0187da88bff337c15a4cd4641323a51fb4dab1a8
nyunja <poptiff@gmail.com>
2024-07-17 12:51:00 +0300

    feat: added a URL path for the output

--- main.go
+++ main.go
@@ -15,9 +15,9 @@ func main() {
 		return
 	}
 
-	//http.HandleFunc("/", Web.FormHandler)
+	// http.HandleFunc("/", Web.FormHandler)
 	http.HandleFunc("/", Web.SubmitFormHandler)
+	http.HandleFunc("/generate", Web.SubmitFormHandler)
 	//http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	log.Fatal(http.ListenAndServe(":8000", nil))
-
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
