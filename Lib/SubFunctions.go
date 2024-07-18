0f75adc2d2123c84906b9581adb0704301442e65
davodhiambo <jesseomolo@yahoo.com>
2024-07-18 15:14:14 +0300

    remove unnecesary print statements from SubFunctions.go

--- Lib/SubFunctions.go
+++ Lib/SubFunctions.go
@@ -18,7 +18,6 @@ func HandleWords(slices []string, words []string) string {
 				output += "\n"
 			}
 		} else {
-			fmt.Print(output)
 			output = HandleCharacters(output, word, slices)
 		}
 	}
@@ -28,15 +27,13 @@ func HandleWords(slices []string, words []string) string {
 // Prints the ASCII ART for each character in each word and updates the output string
 func HandleCharacters(output, word string, slices []string) string {
 	var startIndex int
-	//output := ""
+
 	for i := 0; i < 8; i++ {
 		for _, ch := range word {
 			startIndex = int(ch-32)*9 + 1
 			output += slices[startIndex+i]
-			//fmt.Printf("%s", slices[startIndex+i])
 		}
 		output += "\n"
-		//fmt.Println()
 	}
 	return output
 }
