dabef913568d8f2ee396d1f0cbc63435d710c98d
davodhiambo <jesseomolo@yahoo.com>
2024-07-18 15:45:44 +0300

    documentation: add comments to main.go

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ import (
 )
 
 func main() {
+
+	// Kill server when arguments other than the program name are added
 	if len(os.Args) != 1 {
 		fmt.Println("Too many arguments")
 		return
 	}
 
-	http.HandleFunc("/", Web.SubmitFormHandler)
-	http.HandleFunc("/ascii-art", Web.SubmitFormHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", Web.SubmitFormHandler)          // Serve home page
+	http.HandleFunc("/ascii-art", Web.SubmitFormHandler) // Serve /ascii-art at POST requests
+	log.Fatal(http.ListenAndServe(":8080", nil))         // Defines host port
 }
