1e522246270c9ffb2de5d52cf54f33c0ca0ecb37
davodhiambo <jesseomolo@yahoo.com>
2024-07-22 12:14:39 +0300

    test: add TestHandleWords function to sub_functions_test.go

--- lib/sub_functions_test.go
+++ lib/sub_functions_test.go
@@ -1,5 +1,22 @@
 package lib
 
-import "testing"
+import (
+	"os"
+	"strings"
+	"testing"
+)
 
-func TestHandleWord(t *testing.T)
+func TestHandleWords(t *testing.T) {
+	file, _ := os.ReadFile("../banner-files/standard.txt") // Read standard.txt
+	content := strings.Split(string(file), "\n")           // Split file line-by-line
+
+	subject := []string{"Hello"}
+	got := HandleWords(content, subject)
+	expected := " _    _          _   _          \n| |  | |        | | | |         \n| |__| |   ___  | | | |   ___   \n|  __  |  / _ \\ | | | |  / _ \\  \n| |  | | |  __/ | | | | | (_) | \n|_|  |_|  \\___| |_| |_|  \\___/  \n                                \n                                \n"
+
+	if got != expected {
+		t.Errorf("Got: %s", got)
+		t.Errorf("Expected: %s", expected)
+		t.Errorf("TestHandleWords Failed!")
+	}
+}
